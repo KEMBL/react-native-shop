056f3362fd65275dfe1d6538953267340dbcddbf
KEMBL <kembl07@gmail.com>
2020-11-26 07:20:24 +0100

    removed unused component

--- packages/components/src/views/Delivery/DeliveryManagerScreen/index.tsx
+++ packages/components/src/views/Delivery/DeliveryManagerScreen/index.tsx
@@ -6,7 +6,6 @@ import { translate } from 'localization';
 import { DeliveryScreenTheme } from 'rns-theme';
 import { ui } from 'rns-packages';
 
-import { StylableText } from 'components/src/trivial/text/StylableText';
 import { TopBar } from 'components/src/advanced/TopBar';
 import { DeliveryAddressCard } from 'components/src/advanced/Delivery';
 
